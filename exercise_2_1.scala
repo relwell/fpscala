5ecf3d4e30e7e72c845bb6aee53a959e3950713d
Robert Elwell <robert.elwell@lookout.com>
2015-02-16 17:51:02 -0800

    now with tail recursion

--- exercise_2_1.scala
+++ exercise_2_1.scala
@@ -1,15 +1,19 @@
+import scala.annotation.tailrec
+
 object Exercise_2_1_Module {
 
   def fibonacci( n: Int ): Int = {
-    if (n == 0) 0
-    else if ( n == 1 ) 1
-    else fibonacci(n - 1) + fibonacci(n - 2)
+    @tailrec def go(a: Int, b: Int, n: Int): Int = {
+      if (n > 0) go(b, a + b, n - 1)
+      else a
+    }
+    go(0, 1, n)
   }
 
   def main(args: Array[String] ): Unit = {
-    def go(i: Int): Unit = {
-      if (i > 0) go(i-1)
+    @tailrec def go(i: Int): Unit = {
       println("%d\t%d".format(i, fibonacci(i)))
+      if (i > 0) go(i-1)
     }
     go(args(0).toInt)
 
